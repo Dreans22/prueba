c02d1872fe4ea8b035f074beb45f328cd09678f2
Dreans22 <jaime1w09@gmail.com>
2021-09-14 15:37:04 -0500

    Document.write

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+var numero1= 19;
+var numero2= 16;
+var nombre="andres";
+alert("La edad de "+ nombre+" es de "+numero1+" años");
+
+var numero1=prompt("ingrese el primer numero");
+numero2=prompt  ("ingrese el numero 2");
+var suma=parseInt(numero1)+parseInt(numero2);
+alert("la suma es de: "+ suma);
+document.write("<h4>la suma de los numeros es "+ suma+"</h4>");
