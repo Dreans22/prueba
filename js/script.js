c06c7617112ef3fc20299e864ed3a2dcecd46629
Dreans22 <jaime1w09@gmail.com>
2022-05-17 17:40:13 -0500

    actualizacion

--- js/script.js
+++ js/script.js
@@ -9,3 +9,6 @@ numero2=prompt  ("ingrese el numero 2");
 var suma=parseInt(numero1)+parseInt(numero2);
 alert("la suma es de: "+ suma);
 document.write("<h4>la suma de los numeros es "+ suma+"</h4>");
+
+var numero1_formulario = document.getElementById("numero1").value;
+<form onsubmit="return false"></form>
